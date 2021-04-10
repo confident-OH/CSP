06ee03712d69c829d55e16288148f24c6ef26f72
zeng <confidentoh@foxmail.com>
2021-04-10 21:27:26 +0800

    20180904

--- exercises/CSP/201809/main.cpp
+++ exercises/CSP/201809/main.cpp
@@ -1,69 +1,89 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
 #include <cstdio>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int x[310], limit[310], y[310];
 int n;
+const int Max = 3010;
+int h[Max], ne[Max], e[Max], w[Max], idx;
+bool th[Max];
+int a[Max];
+int sums[Max];
+int in[Max];
+queue<int> q;
 
-bool test(){
-    int item = (x[0]+x[1]) / 2;
-    if(item != y[0]) return false;
-    for(int i = 1; i<n-1; i++){
-        item = (x[i-1]+x[i+1]+x[i]) / 3;
-        if(item != y[i]) return false;
-    }
-    item = (x[n-2] + x[n-1]) / 2;
-    if(item != y[n-1]) return false;
-    return true;
+void init()
+{
+    memset(h, -1, sizeof(h));
+    memset(sums, 0, sizeof(sums));
 }
 
-int dfs(int now)
+void add(int a, int b, int c)
 {
-    x[now] = 1;
-    if(now == n-1){
-        while(x[now]<limit[now]){
-            if(test()){
-                return 1;
-            }
-            else{
-                x[now]++;
-            }
-        }
-        return -1;
+    e[idx] = b; w[idx] = c; ne[idx] = h[a]; h[a] = idx++;
+}
+
+void spfa(int start)
+{
+    queue<int>qq;
+    for(int i = 0; i<=n; i++){
+        qq.push(i);
+        th[i]=true;
+        sums[i]=0;
+        in[i]=1;
     }
-    while (dfs(now+1) == -1){
-        if(x[now]<limit[now]){
-            x[now]++;
-        }
-        else{
-            return -1;
-        }
-    }
-    return 1;
+     while(!qq.empty())
+     {
+         int x=qq.front();
+         qq.pop();
+         in[x]++;
+         th[x]=false;
+         if(in[x]>n){
+            cout<<"noanswer"<<endl;
+            return ;
+         }
+         for(int i=h[x];i!=-1;i=ne[i])
+         {
+             int nx=e[i];
+             if(sums[nx]<sums[x]+w[i])
+             {
+                 sums[nx]=sums[x]+w[i];
+                 if(!th[nx])
+                 {
+                     th[nx]=1;
+                     qq.push(nx);
+                 }
+             }
+         }
+     }
+     return ;
 }
 
 int main()
 {
-    scanf("%d", &n);
-    for(int i = 0; i<n; i++){
-        scanf("%d", &y[i]);
+    init();
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        cin >> a[i];
     }
-    limit[0] = min(2*(y[0] + 1), 3*(y[1] + 1));
-    limit[n - 1] = min(2*(y[n - 1] + 1), 3*(y[n - 2] + 1));
-    if(n>2){
-        limit[1] = min(limit[0], 3*(y[2] + 1));
-        for(int i = 2; i<n-2; i++){
-            limit[i] = min(3*(y[i-1]+1), 3*(y[i]+1));
-            limit[i] = min(limit[i], 3*(y[i+1] +1));
-        }
-        limit[n - 2] = min(limit[n - 1], 3*(y[n - 3] + 1));
-    }
-    dfs(0);
     for(int i = 0; i<n; i++){
-        printf("%d ", x[i]);
+        add(i, i+1, 1);
+    }
+    add(0, 2, 2*a[1]);
+    add(2, 0, -2*a[1] - 1);
+    for(int i = 2; i<n; i++){
+        add(i - 2, i+1, 3*a[i]);
+        add(i+1, i - 2, -3*a[i] - 2);
+    }
+    add(n-2, n, 2*a[n]);
+    add(n, n-2, -2*a[n]-1);
+    spfa(0);
+    for(int i = 0; i<n; i++){
+        cout << sums[i+1] - sums[i] << " ";
     }
     return 0;
 }
