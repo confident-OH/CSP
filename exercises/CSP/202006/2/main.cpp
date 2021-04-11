3ebdb3b6550bb9b4302d31c771953fa5e725dd27
zeng <confidentoh@foxmail.com>
2021-04-11 08:51:53 +0800

    20200602

---
+++ exercises/CSP/202006/2/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+map<int, int>v, u;
+map<int, int>::iterator vitor, uitor;
+int a, u_num, v_num;
+ll result;
+
+int main()
+{
+    scanf("%d %d %d", &a, &v_num, &u_num);
+    int c, d;
+    for(int i = 0; i<v_num; i++){
+        scanf("%d %d", &c, &d);
+        v.insert({c, d});
+    }
+    for(int i = 0; i<u_num; i++){
+        scanf("%d %d", &c, &d);
+        u.insert({c, d});
+    }
+    for(vitor = v.begin(), uitor = u.begin(); vitor!=v.end() && uitor!=u.end(); ){
+        if(vitor->first == uitor->first){
+            result += vitor->second * uitor->second;
+            vitor++;
+            uitor++;
+        }
+        else if(vitor->first > uitor->first){
+            uitor++;
+        }
+        else{
+            vitor++;
+        }
+    }
+    cout << result;
+    return 0;
+}
